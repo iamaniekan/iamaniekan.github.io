31d9f7ffd069abf96e6749fd02e506a85afa9fe5
Aniekan Akpan <aniekanbtc@gmail.com>
2024-06-28 21:53:42 +0100

    Added Personal Contents To Theme

--- js/dark.js
+++ js/dark.js
@@ -2,9 +2,9 @@ var toggle = document.getElementById("dark-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
 
 toggle.addEventListener("click", () => {    
-    if (toggle.className === "btn fas fa-moon") {
+    if (toggle.className === "btn bi bi-toggle-off") {
         setTheme("dark");
-    } else if (toggle.className === "btn fas fa-sun") {
+    } else if (toggle.className === "btn bi bi-toggle-on") {
         setTheme("light");
     }
 });
@@ -13,9 +13,9 @@ function setTheme(mode) {
     localStorage.setItem("dark-mode-storage", mode);
     if (mode === "dark") {
         darkTheme.disabled = false;
-        toggle.className = "btn fas fa-sun";
+        toggle.className = "btn bi bi-toggle-on";
     } else if (mode === "light") {
         darkTheme.disabled = true;
-        toggle.className = "btn fas fa-moon";
+        toggle.className = "btn bi bi-toggle-off";
     }
 }
