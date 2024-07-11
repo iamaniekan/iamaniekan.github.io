966bcd0885f170227b29e7e2ea25d27eec6e31e3
Aniekan Akpan <aniekanbtc@gmail.com>
2024-07-11 19:51:04 +0100

    Dark theme toggler

--- js/dark.js
+++ js/dark.js
@@ -2,9 +2,9 @@ var toggle = document.getElementById("dark-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
 
 toggle.addEventListener("click", () => {    
-    if (toggle.className === "btn bi bi-toggle-off") {
+    if (toggle.className === "btn fas fa-moon") {
         setTheme("dark");
-    } else if (toggle.className === "btn bi bi-toggle-on") {
+    } else if (toggle.className === "btn fas fa-sun") {
         setTheme("light");
     }
 });
@@ -13,9 +13,9 @@ function setTheme(mode) {
     localStorage.setItem("dark-mode-storage", mode);
     if (mode === "dark") {
         darkTheme.disabled = false;
-        toggle.className = "btn bi bi-toggle-on";
+        toggle.className = "btn fas fa-sun";
     } else if (mode === "light") {
         darkTheme.disabled = true;
-        toggle.className = "btn bi bi-toggle-off";
+        toggle.className = "btn fas fa-moon";
     }
 }
